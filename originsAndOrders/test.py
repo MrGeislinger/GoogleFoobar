141031bd1d5def37a35bbdb19400c0d99bf89389
VictorLoren <VictorLoren@users.noreply.github.com>
2016-03-13 10:55:26 -0700

    Test ignores leap days to match the challenge's instructions.

--- originsAndOrders/test.py
+++ originsAndOrders/test.py
@@ -8,7 +8,7 @@ print answer(1,1,99)  #01/01/99
 
 # Create range of dates that are possible
 months = range(1,13)
-days = {1:31,2:29,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
+days = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
 yrs = range(1,100)
 
 # Test different combinations of dates from all month & year combinations
